bd918bf364bb89617531a64c0d622b9d57fbb189
Ann Meier <annmeier3@gmail.com>
2019-07-18 22:23:29 +0000

    Validation Lesson 8.28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, too_short: " is too short..try again" }
+  validates :address, presence: true
+  validates :description, presence: true
 end
+
